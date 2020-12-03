6bad3ce8caed284a30801a34c00b9ec5b88b8dd6
TheEternalShine <36416490+TheEternalShine@users.noreply.github.com>
2020-12-04 01:42:45 +0200

    SOND chunk un/serialization

---
+++ DogScepterLib/Core/Chunks/GMChunkSOND.cs
@@ -0,0 +1,27 @@
+﻿using DogScepterLib.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DogScepterLib.Core.Chunks
+{
+    public class GMChunkSOND : GMChunk
+    {
+        public GMPointerList<GMSound> List;
+
+        public override void Serialize(GMDataWriter writer)
+        {
+            base.Serialize(writer);
+
+            List.Serialize(writer);
+        }
+
+        public override void Unserialize(GMDataReader reader)
+        {
+            base.Unserialize(reader);
+
+            List = new GMPointerList<GMSound>();
+            List.Unserialize(reader);
+        }
+    }
+}
