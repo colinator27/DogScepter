91a4b65d76752d8f23cb443493b30aa4a0cad4e3
TheEternalShine <36416490+TheEternalShine@users.noreply.github.com>
2021-01-03 03:05:58 +0200

    VARI chunk serialization/unserialization

---
+++ DogScepterLib/Core/Chunks/GMChunkVARI.cs
@@ -0,0 +1,62 @@
+﻿using DogScepterLib.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DogScepterLib.Core.Chunks
+{
+    class GMChunkVARI : GMChunk
+    {
+        public List<GMVariable> List;
+
+        public uint InstanceVarCount;
+        public uint MaxLocalVarCount;
+
+        public override void Serialize(GMDataWriter writer)
+        {
+            base.Serialize(writer);
+
+            if (writer.VersionInfo.FormatID > 14)
+            {
+                writer.Write(InstanceVarCount);
+                writer.Write(InstanceVarCount);
+
+                // Set MaxLocalVarCount to highest amount of locals within all entires
+                MaxLocalVarCount = 0;
+                foreach (GMLocalsEntry item in ((GMChunkFUNC)writer.Data.Chunks["FUNC"]).Locals)
+                {
+                    MaxLocalVarCount = (uint)Math.Max(MaxLocalVarCount, item.Entries.Count);
+                }
+
+                writer.Write(MaxLocalVarCount);
+            }
+
+            foreach (GMVariable variable in List)
+            {
+                variable.Serialize(writer);
+            }
+        }
+
+        public override void Unserialize(GMDataReader reader)
+        {
+            base.Unserialize(reader);
+
+            if (reader.VersionInfo.FormatID > 14)
+            {
+                InstanceVarCount = reader.ReadUInt32();
+                reader.Offset += 4;
+                MaxLocalVarCount = reader.ReadUInt32();
+            }
+
+            int varLength = (reader.VersionInfo.FormatID > 14) ? 20 : 12;
+
+            List = new List<GMVariable>();
+            while (reader.Offset + varLength <= StartOffset + Length)
+            {
+                GMVariable gVar = new GMVariable();
+                gVar.Unserialize(reader);
+                List.Add(gVar);
+            }
+        }
+    }
+}
---
+++ DogScepterLib/Core/Models/GMVariable.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DogScepterLib.Core.Models
+{
+    class GMVariable : GMSerializable
+    {
+        public enum VariableTypeEnum : short
+        {
+            Self = -1,
+            Global = -5,
+            NotSpecified = -6,
+            Local = -7,
+            Static = -16
+        }
+
+        public GMString Name;
+        public VariableTypeEnum VariableType;
+        public int VariableID;
+        public uint Occurrences;
+        public uint FirstAddress; // TODO: Should be a bytecode instruction
+
+        public void Serialize(GMDataWriter writer)
+        {
+            writer.WritePointerString(Name);
+
+            if (writer.VersionInfo.FormatID > 14)
+            {
+                writer.Write((int)VariableType);
+                writer.Write(VariableID);
+            }
+            writer.Write(Occurrences);
+            if (Occurrences > 0)
+                writer.Write(FirstAddress);
+            else
+                writer.Write((int)-1);
+        }
+
+        public void Unserialize(GMDataReader reader)
+        {
+            Name = reader.ReadStringPointerObject();
+
+            if (reader.VersionInfo.FormatID > 14)
+            {
+                VariableType = (VariableTypeEnum)reader.ReadInt32();
+                VariableID = reader.ReadInt32();
+            }
+            Occurrences = reader.ReadUInt32();
+            if (Occurrences > 0)
+            {
+                FirstAddress = reader.ReadUInt32();
+            }
+            else
+            {
+                if (reader.ReadInt32() != -1)
+                    reader.Warnings.Add(new GMWarning("Variable with no occurrences, but still has a first occurrence address"));
+                FirstAddress = 0;
+            }
+        }
+    }
+}
