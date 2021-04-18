8b8026f5ca0b8f5d3c60a729dfad7054222bf2f5
colinator27 <colinator27@users.noreply.github.com>
2021-04-17 21:06:29 -0400

    Some texture utilities

--- DogScepterLib/Project/Textures.cs
+++ DogScepterLib/Project/Textures.cs
@@ -1,5 +1,6 @@
 ﻿using DogScepterLib.Core.Chunks;
 using DogScepterLib.Core.Models;
+using SkiaSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,7 @@ namespace DogScepterLib.Project
         public ProjectFile Project;
         public List<HashSet<int>> TextureGroupPages = new List<HashSet<int>>();
         public Dictionary<int, int> PageToGroup = new Dictionary<int, int>();
+        public Dictionary<int, SKBitmap> CachedTextures = new Dictionary<int, SKBitmap>();
 
         public Textures(ProjectFile project)
         {
@@ -88,6 +90,48 @@ namespace DogScepterLib.Project
                 foreach (int page in TextureGroupPages[i])
                     PageToGroup[page] = i;
             }
+        }
+
+        public SKBitmap GetTexture(int ind)
+        {
+            if (CachedTextures.TryGetValue(ind, out SKBitmap res))
+                return res;
+            return CachedTextures[ind] = SKBitmap.Decode(Project.DataHandle.GetChunk<GMChunkTXTR>().List[ind].TextureData.Data);
+        }
+
+        public SKBitmap GetTextureEntryBitmap(GMTextureItem entry)
+        {
+            if (entry.TargetX == 0 && entry.TargetY == 0 &&
+                entry.SourceWidth == entry.BoundWidth &&
+                entry.SourceHeight == entry.BoundHeight)
+            {
+                // This can be done without copying
+                return GetTextureEntryBitmapFast(entry);
+            }
+
+            SKBitmap texture = GetTexture(entry.TexturePageID);
+            SKBitmap res = new SKBitmap(entry.BoundWidth, entry.BoundHeight, texture.ColorType, texture.AlphaType);
+            using (SKCanvas canvas = new SKCanvas(res))
+            {
+                canvas.Clear();
+                canvas.DrawBitmap(texture, new SKRectI(entry.SourceX, entry.SourceY,
+                                                       entry.SourceX + entry.SourceWidth, entry.SourceY + entry.SourceHeight),
+                                           new SKRectI(entry.TargetX, entry.TargetY,
+                                                       entry.TargetX + entry.TargetWidth, entry.TargetY + entry.TargetHeight));
+            }
+
+            return res;
+        }
+
+        // Quickly gets a view into a subset of the bitmap, rather than copying and making room for potential padding
+        public SKBitmap GetTextureEntryBitmapFast(GMTextureItem entry)
+        {
+            SKBitmap texture = GetTexture(entry.TexturePageID);
+            SKBitmap res = new SKBitmap();
+            if (texture.ExtractSubset(res, new SKRectI(entry.SourceX, entry.SourceY, 
+                                                       entry.SourceX + entry.SourceWidth, entry.SourceY + entry.SourceHeight)))
+                return res;
+            return null;
         }
     }
 }
