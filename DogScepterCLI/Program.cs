f02be61c760b2593b64bbd54c0a932524778c020
colinator27 <colinator27@users.noreply.github.com>
2022-02-25 14:13:34 -0500

    Second attempt build workflows

--- DogScepterCLI/Program.cs
+++ DogScepterCLI/Program.cs
@@ -9,6 +9,8 @@ namespace DogScepterCLI
         public static async Task<int> Main() =>
             await new CliApplicationBuilder()
                 .SetTitle("DogScepter CLI")
+                .SetExecutableName("DogScepterCLI")
+                .SetVersion("0.0.1")
                 .SetDescription("Interface for working with data and project files.")
                 .AddCommandsFromThisAssembly()
                 .Build()
