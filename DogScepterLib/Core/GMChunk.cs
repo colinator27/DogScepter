63a86138e7908c8cd9cda70d4b3ef4b73acd7db7
Miepee <38186597+Miepee@users.noreply.github.com>
2022-04-08 16:17:27 +0200

    Document GMChunk

--- DogScepterLib/Core/GMChunk.cs
+++ DogScepterLib/Core/GMChunk.cs
@@ -1,155 +1,141 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-
 using DogScepterLib.Core.Chunks;
 
-namespace DogScepterLib.Core
+namespace DogScepterLib.Core;
+
+/// <summary>
+/// A class that represents a chunk in a GameMaker data file.
+/// </summary>
+public class GMChunk : IGMSerializable
 {
-    public class GMChunk : IGMSerializable
+    public int Length;
+    public int StartOffset;
+    public int EndOffset;
+
+    /// <summary>
+    /// Serializes a GameMaker chunk into a specified <see cref="GMDataWriter"/>.
+    /// </summary>
+    /// <param name="writer">The <see cref="GMDataWriter"/> from where to serialize to.</param>
+    public virtual void Serialize(GMDataWriter writer)
     {
-        public int Length;
-        public int StartOffset;
-        public int EndOffset;
-
-        public virtual void Serialize(GMDataWriter writer)
-        {
-        }
-
-        public virtual void Deserialize(GMDataReader reader)
-        {
-            // Read chunk length, measure start/end
-            Length = reader.ReadInt32();
-            StartOffset = reader.Offset;
-            EndOffset = StartOffset + Length;
-        }
     }
 
     /// <summary>
-    /// The chunk that contains all other chunks.
+    /// Deserializes a GameMaker chunk from a specified <see cref="GMDataReader"/>.
     /// </summary>
-    public class GMChunkFORM : GMChunk
+    /// <param name="reader">The <see cref="GMDataReader"/> from where to deserialize from.</param>
+    public virtual void Deserialize(GMDataReader reader)
     {
-        public List<string> ChunkNames;
-        public Dictionary<string, GMChunk> Chunks;
+        // Read chunk length, measure start/end
+        Length = reader.ReadInt32();
+        StartOffset = reader.Offset;
+        EndOffset = StartOffset + Length;
+    }
+}
 
-        public static readonly Dictionary<string, Type> ChunkMap = new Dictionary<string, Type>()
+/// <summary>
+/// The chunk of a GameMaker data file that contains all other chunks.
+/// </summary>
+public class GMChunkFORM : GMChunk
+{
+    //TODO: make this <GMChunkNames, GMChunk> (not string) in order to mitigate typos?
+    public List<string> ChunkNames;
+    public Dictionary<string, GMChunk> Chunks;
+
+    public static readonly Dictionary<string, Type> ChunkMap = new Dictionary<string, Type>()
+    {
+        { "GEN8", typeof(GMChunkGEN8) },
+        { "OPTN", typeof(GMChunkOPTN) },
+        { "LANG", typeof(GMChunkLANG) },
+        { "EXTN", typeof(GMChunkEXTN) },
+        { "STRG", typeof(GMChunkSTRG) },
+        { "TPAG", typeof(GMChunkTPAG) },
+        { "TXTR", typeof(GMChunkTXTR) },
+        { "AGRP", typeof(GMChunkAGRP) },
+        { "AUDO", typeof(GMChunkAUDO) },
+        { "SOND", typeof(GMChunkSOND) },
+        { "BGND", typeof(GMChunkBGND) },
+        { "PATH", typeof(GMChunkPATH) },
+        { "EMBI", typeof(GMChunkEMBI) },
+        { "DAFL", typeof(GMChunkDAFL) },
+        { "TGIN", typeof(GMChunkTGIN) },
+        { "FONT", typeof(GMChunkFONT) },
+        { "SPRT", typeof(GMChunkSPRT) },
+        { "ACRV", typeof(GMChunkACRV) },
+        { "FUNC", typeof(GMChunkFUNC) },
+        { "VARI", typeof(GMChunkVARI) },
+        { "SCPT", typeof(GMChunkSCPT) },
+        { "TAGS", typeof(GMChunkTAGS) },
+        { "ROOM", typeof(GMChunkROOM) },
+        { "OBJT", typeof(GMChunkOBJT) },
+        { "TMLN", typeof(GMChunkTMLN) },
+        { "GLOB", typeof(GMChunkGLOB) },
+        { "GMEN", typeof(GMChunkGMEN) },
+        { "SHDR", typeof(GMChunkSHDR) },
+        { "CODE", typeof(GMChunkCODE) },
+        { "SEQN", typeof(GMChunkSEQN) },
+        { "FEDS", typeof(GMChunkFEDS) },
+    };
+    public static readonly Dictionary<Type, string> ChunkMapReverse = ChunkMap.ToDictionary(x => x.Value, x => x.Key);
+
+    public override void Serialize(GMDataWriter writer)
+    {
+        base.Serialize(writer);
+
+        int beg = writer.BeginLength();
+
+        // Write all the sub-chunks
+        for (int i = 0; i < ChunkNames.Count; i++)
         {
-            { "GEN8", typeof(GMChunkGEN8) },
-            { "OPTN", typeof(GMChunkOPTN) },
-            { "LANG", typeof(GMChunkLANG) },
-            { "EXTN", typeof(GMChunkEXTN) },
-            { "STRG", typeof(GMChunkSTRG) },
-            { "TPAG", typeof(GMChunkTPAG) },
-            { "TXTR", typeof(GMChunkTXTR) },
-            { "AGRP", typeof(GMChunkAGRP) },
-            { "AUDO", typeof(GMChunkAUDO) },
-            { "SOND", typeof(GMChunkSOND) },
-            { "BGND", typeof(GMChunkBGND) },
-            { "PATH", typeof(GMChunkPATH) },
-            { "EMBI", typeof(GMChunkEMBI) },
-            { "DAFL", typeof(GMChunkDAFL) },
-            { "TGIN", typeof(GMChunkTGIN) },
-            { "FONT", typeof(GMChunkFONT) },
-            { "SPRT", typeof(GMChunkSPRT) },
-            { "ACRV", typeof(GMChunkACRV) },
-            { "FUNC", typeof(GMChunkFUNC) },
-            { "VARI", typeof(GMChunkVARI) },
-            { "SCPT", typeof(GMChunkSCPT) },
-            { "TAGS", typeof(GMChunkTAGS) },
-            { "ROOM", typeof(GMChunkROOM) },
-            { "OBJT", typeof(GMChunkOBJT) },
-            { "TMLN", typeof(GMChunkTMLN) },
-            { "GLOB", typeof(GMChunkGLOB) },
-            { "GMEN", typeof(GMChunkGMEN) },
-            { "SHDR", typeof(GMChunkSHDR) },
-            { "CODE", typeof(GMChunkCODE) },
-            { "SEQN", typeof(GMChunkSEQN) },
-            { "FEDS", typeof(GMChunkFEDS) },
-        };
-        public static readonly Dictionary<Type, string> ChunkMapReverse = ChunkMap.ToDictionary(x => x.Value, x => x.Key);
-
-        public override void Serialize(GMDataWriter writer)
-        {
-            base.Serialize(writer);
-
-            int beg = writer.BeginLength();
-
-            // Write all the sub-chunks
-            for (int i = 0; i < ChunkNames.Count; i++)
+            GMChunk chunk;
+            if (Chunks.TryGetValue(ChunkNames[i], out chunk))
             {
-                GMChunk chunk;
-                if (Chunks.TryGetValue(ChunkNames[i], out chunk))
-                {
-                    writer.Data.Logger?.Invoke($"Writing {ChunkNames[i]} at {writer.Offset:X}");
+                writer.Data.Logger?.Invoke($"Writing {ChunkNames[i]} at {writer.Offset:X}");
 
-                    // Write chunk name, length, and content
-                    writer.Write(ChunkNames[i].ToCharArray());
-                    int chunkBeg = writer.BeginLength();
-                    chunk.Serialize(writer);
+                // Write chunk name, length, and content
+                writer.Write(ChunkNames[i].ToCharArray());
+                int chunkBeg = writer.BeginLength();
+                chunk.Serialize(writer);
 
-                    // If not the last chunk, apply 16-byte padding if necessary
-                    if (writer.Data.VersionInfo.AlignChunksTo16 && i != (ChunkNames.Count - 1))
-                        writer.Pad(16);
+                // If not the last chunk, apply 16-byte padding if necessary
+                if (writer.Data.VersionInfo.AlignChunksTo16 && i != (ChunkNames.Count - 1))
+                    writer.Pad(16);
 
-                    writer.EndLength(chunkBeg);
-                }
+                writer.EndLength(chunkBeg);
             }
-
-            writer.EndLength(beg);
         }
 
-        public override void Deserialize(GMDataReader reader)
+        writer.EndLength(beg);
+    }
+
+    public override void Deserialize(GMDataReader reader)
+    {
+        base.Deserialize(reader);
+
+        // Gather the names and offsets of the sub-chunks
+        ChunkNames = new List<string>();
+        //TODO: updated, but never used
+        List<int> ChunkOffsets = new List<int>();
+        while (reader.Offset < EndOffset)
         {
-            base.Deserialize(reader);
+            // Read its name and skip contents
+            ChunkOffsets.Add(reader.Offset);
+            string name = reader.ReadChars(4);
+            ChunkNames.Add(name);
+            int length = reader.ReadInt32();
+            reader.Offset += length;
 
-            // Gather the names and offsets of the sub-chunks
-            ChunkNames = new List<string>();
-            List<int> ChunkOffsets = new List<int>();
-            while (reader.Offset < EndOffset)
-            {
-                // Read its name and skip contents
-                ChunkOffsets.Add(reader.Offset);
-                string name = reader.ReadChars(4);
-                ChunkNames.Add(name);
-                int length = reader.ReadInt32();
-                reader.Offset += length;
+            // Check if this is a GMS 2.3+ file
+            if (name == "SEQN")
+                reader.Data.VersionInfo.SetVersionNumber(2, 3);
+            else if (name == "FEDS")
+                reader.Data.VersionInfo.SetVersionNumber(2, 3, 6);
 
-                // Check if this is a GMS 2.3+ file
-                if (name == "SEQN")
-                    reader.Data.VersionInfo.SetVersionNumber(2, 3);
-                else if (name == "FEDS")
-                    reader.Data.VersionInfo.SetVersionNumber(2, 3, 6);
-
-                // Update whether this version aligns chunks to 16 bytes
-                if (reader.Offset < EndOffset)
-                    reader.Data.VersionInfo.AlignChunksTo16 &= (reader.Offset % 16 == 0);
-            }
-
-            // Actually read all the sub-chunks
-            Chunks = new Dictionary<string, GMChunk>();
-            reader.Offset = StartOffset;
-            for (int i = 0; i < ChunkNames.Count; i++)
-            {
-                reader.Offset = ChunkOffsets[i];
-                reader.Data.Logger?.Invoke($"Reading {ChunkNames[i]} at {reader.Offset:X}");
-
-                Type type;
-                if (!ChunkMap.TryGetValue(ChunkNames[i], out type))
-                {
-                    // Unknown chunk name, so skip past it
-                    reader.Warnings.Add(new GMWarning($"Unknown chunk with name {ChunkNames[i]}",
-                                                        GMWarning.WarningLevel.Severe, GMWarning.WarningKind.UnknownChunk));
-                    continue;
-                }
-
-                // Actually parse the chunk, starting at its length
-                reader.Offset += 4;
-                GMChunk chunk = (GMChunk)Activator.CreateInstance(type);
-                Chunks.Add(ChunkNames[i], chunk);
-                chunk.Deserialize(reader);
-            }
+            // Update whether this version aligns chunks to 16 bytes
+            if (reader.Offset < EndOffset)
+                reader.Data.VersionInfo.AlignChunksTo16 &= (reader.Offset % 16 == 0);
         }
     }
 }
